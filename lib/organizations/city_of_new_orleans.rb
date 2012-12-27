62dc05905eacd64cac44b10907ebd3cc8b1bfcdf
Eddie A Tejeda <eddie@codeforamerica.org>
2012-12-27 18:06:25 -0500

    set template for cities

--- lib/organizations/city_of_new_orleans.rb
+++ lib/organizations/city_of_new_orleans.rb
@@ -1,13 +1,33 @@
 namespace :new_orleans do
-	desc "Rake task to call other rake tasks with proper options"
-	task :initialize do
+
+
+  desc "New Orleans: load addresses"
+  task :load_addresses do
+
+    # load addresses
     address_shapefile_url = 'https://data.nola.gov/api/file_data/Gn9aLqlGx_9jR-DzakSNiXu3Y5iO1YvL5O8XPgIj6no?filename=NOLA_Addresses_20121214.zip'
-		Rake::Task["addresses:load address_shapefile_url=#{address_shapefile_url}"].invoke
-	end
+    Rake::Task["addresses:load shapefile=#{address_shapefile_url}"].invoke
 
+    # load neighborhoods
 
-  task :update do
-    Rake::Task["addresses:update"].invoke
   end
+
+  desc "New Orleans: tasks that run daily"
+  task :scheduled_daily do
+
+  end
+
+  desc "New Orleans: tasks that run weekly"
+  task :scheduled_weekly do
+
+  end
+
+
+  desc "New Orleans: tasks that run monthly"
+  task :scheduled_monthly do
+
+  end
+
+
 
 end

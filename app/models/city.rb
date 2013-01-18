f75575c5790c9d544768b75b783f6e415576f9fd
amirbey <amirbey@gmail.com>
2013-01-17 20:14:45 -0800

    remove city models

--- app/models/city.rb
+++
@@ -1,6 +0,0 @@
-class City < ActiveRecord::Base
-  # attr_accessible :title, :body
-  belongs_to :state
-  has_many :city_workflows
-  has_many :workflows, :through => :city_workflows
-end
--- app/models/city_workflow.rb
+++
@@ -1,6 +0,0 @@
-class CityWorkflow < ActiveRecord::Base
-  # attr_accessible :title, :body
-  belongs_to :city
-  belongs_to :workflow
-  has_many :cases
-end
